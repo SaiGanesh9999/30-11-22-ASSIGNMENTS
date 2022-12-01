0b39f2df506f2aafeff030b524fed71459393d4d
SaiGanesh9999 <110873642+SaiGanesh9999@users.noreply.github.com>
2022-12-01 06:28:42 +0530

    Create ASSIGNMENT1.py

---
+++ ASSIGNMENT1.py
@@ -0,0 +1,28 @@
+TomatosPrice=20;CarrotsPrice=50;OnionsPrice=60;BeansPrice=40;PotatosPrice=30;
+Phone =int(input("Enter Customer Phone Number : "))
+Name =     input("Enter Customer Name         : ")
+Address =  input("Enter Customer Address      : ")
+Tomatos = int(input("Enter Quantity Of Tomatos : "))
+Carrots = int(input("Enter Quantity Of Carrots : "))
+Onions =  int(input("Enter Quantity Of Onions  : "))
+Beans =   int(input("Enter Quantity Of Beans   : "))
+Potatos = int(input("Enter Quantity Of Potatos : "))
+Price=Tomatos*TomatosPrice+Carrots*CarrotsPrice+Onions*OnionsPrice+Beans*BeansPrice+Potatos*PotatosPrice
+Tax=0
+if Price <= 500 :
+    Tax = Price/5
+elif Price <= 1000 :
+    Tax = Price/5
+elif Price <= 2000 :
+    Tax = Price/5
+elif Price <= 3000 :
+    Tax = Price/5
+else :
+    Tax = 100
+Bill = Price+Tax
+print("*****************************************************************************")
+print("              Customer Name     :",Name)
+print("              Customer Phone No :",Phone)
+print("              Tax               :",Tax)
+print("              Bill              :",Bill)
+print("*****************************************************************************")
